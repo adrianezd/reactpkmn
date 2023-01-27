287a1d3b9c93aa692bbf6b4c071f33ffda54de50
adrianezd <adrianezquerrods@gmail.com>
2023-01-27 19:41:04 +0100

    Updates

--- src/services/getAllPokemonData.js
+++
@@ -1,9 +0,0 @@
-export default function getAllPokemonsData() {
-  const url = `https://pokeapi.co/api/v2/pokemon?limit=151`;
-  return fetch(url)
-    .then((res) => res.json())
-    .then(function (response) {
-      const pokemons = response.results;
-      return pokemons;
-    });
-}
--- src/services/getPokemon.js
+++
@@ -1,9 +0,0 @@
-export default function Pokemon({ id = "90" } = {}) {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  return fetch(url)
-    .then((res) => res.json())
-    .then(function (response) {
-      const pokemon = response.name;
-      return pokemon;
-    });
-}
